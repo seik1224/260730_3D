e91f1fec4ee1e06bef6bcbc8cf83c9c0cf5bd0a2
seik-G <seik1224@naver.com>
2026-07-30 17:46:39 +0900

    .

--- README.md
+++ README.md
@@ -1,36 +1,10 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# Three.js/ Fiber / drei
 
-## Getting Started
+Three.js : 브라우저에서 3D 그래픽을 쉽게 렌더링할 수 있게 도와주는 javascript 라이브러리
 
-First, run the development server:
+Fiber : Three.js를 React내에서 사용할 수 있게 도와주는 라이브러리
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+Drei : Fiber를 기반으로 한 유틸리티 모음 라이브러리
+(자주 사용되는 3D기능들을 미리 컴포넌트로 만들어 놓아 개발 생산성을 높여줌)
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+설치 : npm i three @react-three/fiber @react-three/drei
---
+++ src/app/three/three01.tsx
@@ -0,0 +1,64 @@
+import { Cone, Sphere } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+
+const Three01 = () => {
+    /*
+        [ mesh ]
+        - Three.js의 Mesh객체
+        - 기하학(geometry)과 재질(material)을 조합하여 3D 객체를 생성할때 사용
+        - 일반적으로 geometry와 material을 자식으로 가짐
+
+        <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+        </mesh>
+
+        [ primitive ]
+        - 더 일반적인 용도 사용
+        - Three.js의 모든 객체타입을 래핑가능
+        (메시, 그룹, 카메라, 라이트 등...)
+        - 자체적으로 Three.js 객체를 포함하며, 일반적으로 자식요소를 가지지 않음
+
+        [ group ]
+        - three.js의 Group 객체에 해당 ( 여러 3D 객체를 그룹화하는데 사용 )
+        - 다른 3D 객체들을 자식으로 포함할 수 있음
+
+    */
+
+    return (
+        <>
+            <div className='h-dvh'>
+                {/* Fiber : 3D씬을 렌더링하는 공간을 제공 */}
+                <Canvas>
+                    {/* 주변광추가 */}
+                    <ambientLight intensity={3} />
+
+                    {/* mesh : 3D객체의 기본 단위 */}
+                    <mesh position={[-2, 0, 0]}>
+                        {/* 반지름, 세그먼트수, 세그먼트수 */}
+                        <sphereGeometry args={[1, 12, 12]} />
+                        <meshStandardMaterial color='red' />
+                    </mesh>
+                    {/* 
+                        [ drei의 primitive 컴포넌트 ]
+                        - mesh 래퍼가 필요없음(각 컴포넌트가 이미 mesh를 포함하고 있음)
+                        - geometry와 material을 자동으로 생성하며, custom material을 자식으로 추가 가능
+                        - position, rotation, scale속성을 직접 받을 수 있음
+                        - args prop을 통해 geometry의 인수를 전달가능
+                    */}
+                    <Sphere position={[-4,0,0]} args={[1,4,4]}>
+                        <meshStandardMaterial color='red' />
+                    </Sphere>
+
+                    <Cone position={[2,0,0]} args={[1,2,32]}>
+                        <meshStandardMaterial color='green' />
+                    </Cone>
+
+                </Canvas>
+            </div>
+        </>
+    )
+}
+
+export default Three01
