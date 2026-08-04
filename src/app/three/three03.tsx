fd0c3b48d16a1c1a72fe9b22c8701e202366120f
seik-G <seik1224@naver.com>
2026-08-04 17:15:43 +0900

    .

--- src/app/three/three03.tsx
+++ src/app/three/three03.tsx
@@ -1,9 +1,21 @@
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
 const Three03 = () => {
-  return (
-    <div>Three03</div>
-  )
+    return (
+        <>
+            <div className='h-dvh'>
+                <Canvas>
+                    <ambientLight intensity={10} />
+
+                    <mesh position={[0,0,0]}>
+                        <boxGeometry args={[1,1,1]} />
+                        <meshStandardMaterial color={'ornage'} />
+                    </mesh>
+                </Canvas>
+            </div>
+        </>
+    )
 }
 
 export default Three03
